a2c4c2377efdcb5d89e267b409fc4f898cd145db
MJ <whe2266@naver.com>
2023-05-30 12:44:19 +0900

    user 정규식표현 추가

--- Models/BookUser.cs
+++ Models/BookUser.cs
@@ -8,22 +8,24 @@ public class BookUser
     [Key]
     public int USER_NUM { get; set; }
 
-    [Required(ErrorMessage = "사용자 이름을 입력하세요.")]
+    [Required(ErrorMessage = "사용자 이름을 입력하세요."), StringLength(5)]
     public string USER_NAME { get; set; }
 
-    [Required(ErrorMessage = "사용자 아이디를 입력하세요.")]
+    [Required(ErrorMessage = "사용자 아이디를 입력하세요."), StringLength(15)]
     public string USER_ID { get; set; }
 
-    [Required(ErrorMessage = "사용자 비밀번호를 입력하세요.")]
+    [Required(ErrorMessage = "사용자 비밀번호를 입력하세요.")] //정규표현식 사용
+    [RegularExpression("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?':{}|<>])[A-Za-z0-9!@#$%^&*(),.?':{}|<>]+$"
+    , ErrorMessage ="영어와 숫자, 특수문자를 사용해 주세요"), StringLength(15)]
     public string USER_PW { get; set; }
 
     [Required(ErrorMessage = "사용자 분류를 선택하세요.")]
     public string USER_TYPE { get; set; }
 
     [Required(ErrorMessage = "EMAIL을 입력하세요")]
-    [EmailAddress]
+    [EmailAddress(ErrorMessage = "올바른 EMAIL 형식이 아닙니다"), StringLength(25)]
     public string USER_EMAIL { get; set; }
 
-    [Required(ErrorMessage = "출판사명을 입력하세요")]
+    [Required(ErrorMessage = "출판사명을 입력하세요"), StringLength(15)]
     public string PUBLISHER { get; set; }
 }
--- Models/PartnerManagement.cs
+++ Models/PartnerManagement.cs
@@ -6,6 +6,7 @@ public class PartnerManagement
 {
     [Key]
     public int PM_NUM { get; set; }
+    [RegularExpression(@"^\+?[0-9]{1,3}-?[0-9]{1,4}-?[0-9]{1,4}$", ErrorMessage = "올바른 전화번호 형식을 입력해주세요.")]
     public string PM_CONTACT { get; set; }
     public string PUBLISHER { get; set; }
     public string PM_ADD { get; set; }
