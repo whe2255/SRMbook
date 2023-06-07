e7a5bfaaf96d74539c0c6a00a5b2f89c034e7165
MJ <whe2266@naver.com>
2023-06-07 19:18:34 +0900

    도서 분류, 재고 완료

--- Models/BookInventory.cs
+++ Models/BookInventory.cs
@@ -7,10 +7,21 @@ public class BookInventory
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Display(Name = "도서 번호")]
     public int BOOK_NUM { get; set; }
+
+    [Display(Name = "분류명")]
     public string BOOK_CLASS { get; set; }
+
+    [Display(Name = "도서명")]
     public string BOOK_NAME { get; set; }
+
+    [Display(Name = "저자")]
     public string BOOK_WRITER { get; set; }
+
+    [Display(Name = "가격")]
     public int BOOK_PRICE { get; set; }
+
+    [Display(Name = "재고 수량")]
     public int BOOK_QUANTITY { get; set; }
 }
